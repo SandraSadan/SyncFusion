645975abbb5e7337886a85569cd992cd93b79eee
N-Suvedha <suvedha96@gmail.com>
2022-03-15 16:38:01 +0530

    review changes donw

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -2,6 +2,4 @@ export default () => ({
     port: Number(process.env.PORT),
     backendPrefix: Number(process.env.BACKEND_PREFIX),
     socketPort: process.env.SOCKET_PORT,
-    FrontendUrl: Number(process.env.FRONTEND_URL),
 });
-  
