ad995dce9eb027028ec2e97f4d32268a07eab994
kathiask2 <kathirask@hotmail.com>
2022-03-31 15:38:36 +0530

    parentId issue fixed

--- server/src/shared/utils/constants.ts
+++ server/src/shared/utils/constants.ts
@@ -2,11 +2,12 @@ export enum File {
   ADD_ROW = 'add_row',
   UPDATE_ROW = 'update_row',
   DELETE_ROW = 'delete_row',
+  PASTE_CHILD = 'paste_child',
   PASTE_ROW = 'paste_row',
   UPDATE_COLUMN = 'update_column',
   DELETE_COLUMN = 'delete_column',
   CREATE_COLUMN = 'create_column',
   GET_ROW = 'get_row',
   GET_COLUMN = 'get_column',
-  REMOVE_PROPERTY = 'remove_property'
+  REMOVE_PROPERTY = 'remove_property',
 }
